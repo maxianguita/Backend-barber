9edf04be42fcd49ec6dcf97704bda7f529c26eba
Maxi Anguita <maxianguita@MacBook-Pro-de-Maxi.local>
2025-06-24 18:30:30 -0300

    probando variables

--- config/database.js
+++ config/database.js
@@ -33,10 +33,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production' ? {
-      require: true,
-      rejectUnauthorized: false,
-    } : false,
+    ssl: process.env.NODE_ENV === 'production'
+      ? {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      : false,
   },
   logging: false,
 });
