3c5b4e78f01da4ccf735672d032c68deecc3f3da
Maxi Anguita <maxianguita@MacBook-Pro-de-Maxi.local>
2025-06-24 15:06:43 -0300

    nuevos cambios db

--- config/database.js
+++ config/database.js
@@ -26,21 +26,34 @@
 // testConnection();
 
 // module.exports = sequelize;
-// config/db.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+const sequelize = isProduction
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        logging: false,
+      }
+    );
 
 async function testConnection() {
   try {
